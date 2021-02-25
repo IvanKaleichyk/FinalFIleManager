82303d4b7da4a313d68de5a8236eb815c6425191
KoleychikIvan <64867022+KoleychikIvan@users.noreply.github.com>
2021-02-25 17:43:42 +0100

    testLoading

--- app/src/main/java/com/koleychik/finalfilemanager/MainActivity.kt
+++ app/src/main/java/com/koleychik/finalfilemanager/MainActivity.kt
@@ -4,12 +4,14 @@ import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.koleychik.finalfilemanager.AppConstants.LAST_DESTINATION_ID
 import com.koleychik.finalfilemanager.AppConstants.START_FRAGMENT_ID
 import com.koleychik.finalfilemanager.navigation.Navigator
+import com.koleychik.finalfilemanager.test.TestLoadingFragment
 import com.koleychik.module_injector.AppConstants
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.AppSettingsDialog
@@ -36,6 +38,12 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
         setTheme(R.style.Theme_FinalFIleManager)
         super.onCreate(savedInstanceState)
         App.component.inject(this)
+
+        // TODO TEST START
+        findViewById<Button>(R.id.btnTestLoading).setOnClickListener {
+            startActivity(Intent(applicationContext, TestLoadingFragment::class.java))
+        }
+        // TODO TEST END
 
         getStartFragment(savedInstanceState)
         Log.d(tag, "MainActivity onCreate")
--- app/src/main/java/com/koleychik/finalfilemanager/di/AppComponent.kt
+++ app/src/main/java/com/koleychik/finalfilemanager/di/AppComponent.kt
@@ -6,6 +6,7 @@ import com.koleychik.finalfilemanager.di.modules.ApiModule
 import com.koleychik.finalfilemanager.di.modules.AppModule
 import com.koleychik.finalfilemanager.di.modules.DependenciesModule
 import com.koleychik.finalfilemanager.di.modules.DestroyerModule
+import com.koleychik.finalfilemanager.test.TestLoadingFragment
 import dagger.Component
 import javax.inject.Singleton
 
@@ -21,4 +22,5 @@ interface AppComponent {
 
     fun inject(activity: MainActivity)
     fun inject(app: App)
+    fun inject(activity: TestLoadingFragment)
 }
